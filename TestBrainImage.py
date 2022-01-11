35d529626e4ed3cd8eef90db2e0320d47e664fbc
Anna Henderson <amh343@cornell.edu>
2022-01-11 11:35:31 -0500

    rename symbols and make function to display plot

--- TestBrainImage.py
+++ TestBrainImage.py
@@ -10,50 +10,52 @@ import seaborn as sns
 from scipy.signal._peak_finding import argrelextrema
 from random import randint
 
+
+def display_hist(minima):
+    '''Display intensity histogram with vertical lines marking each [minima] value. Print minima values.
+    '''
+    for n in minima:
+        plt.axvline(x=n)
+    print(minima)
+    plt.show()
+
+
 # TODO: Allow user to input image
 img = cv2.imread('Images/BrainNoSkull.png', 0)
 org_img = cv2.imread('Images/BrainNoSkull.png', 0)
 
-# PROBABILITY DENSITY PLOT
 # flatten image, remove black (background )
 flat_img = img.ravel()
 clean_data = np.delete(flat_img, np.where(flat_img == 0))
+
+# PROBABILITY DENSITY PLOT
 # plot data in histogram with a gaussian kernel density estimate
 p = sns.distplot(clean_data, hist=True, kde=True, bins=list(range(1, 255)))
 
 # FIND MINIMA
 # make a list of the frequencies in the displot
-l = [h.get_height() for h in p.patches]
+frequencies = [h.get_height() for h in p.patches]
 # convert list of frequencies to numpy array
-new_l = np.asarray(l)
-# find maxima, order tells how may points on either side to use for the comparator (ex. greater)
-# future work: calculate order so that it will work for any image
-minm = argrelextrema(new_l, np.less, order=20)
-# returns tuple
-# minm[k] is the array of indices of axis k of data
-# convert minm on axis 0 (the only axis) to list
-minm_l = minm[0].tolist()
-for n in minm_l:
-    # add lines to plot so I can see
-    plt.axvline(x=n)
-# show displot
-print(minm_l)
-plt.show()
+np_freq = np.asarray(frequencies)
+# find minima
+# TODO: calculate order so that it will work for any image
+minm = argrelextrema(np_freq, np.less, order=20)
+minima = minm[0].tolist()
 
 # IMAGE MANIPULATION
 # TODO: find better values for thresholding besides random intensities
-csf = randint(25, minm_l[1])
-gm = randint(minm_l[1]+1, minm_l[2])
+csf = randint(25, minima[1])
+gm = randint(minima[1]+1, minima[2])
 wm = 255
 # height
 for up in range(img.shape[0]):
     for across in range(img.shape[1]):
         selected = img[up][across]
-        if selected < minm_l[1] and selected != 0:
+        if selected < minima[1] and selected != 0:
             img[up][across] = csf
-        elif selected > minm_l[2]:
+        elif selected > minima[2]:
             img[up][across] = wm
-        elif selected > minm_l[1] and selected < minm_l[2]:
+        elif selected > minima[1] and selected < minima[2]:
             img[up][across] = gm
 # concatenate images horizontally
 img_concat = np.concatenate((img, org_img), axis=1)
